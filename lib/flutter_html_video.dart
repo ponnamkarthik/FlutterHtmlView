8ca74a9689b3be95fe410d2f785aefca4f0ccb89
marekchen <marek@163.com>
2018-05-24 09:52:59 +0800

    add support for <video> tag

---
+++ lib/flutter_html_video.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayPause extends StatefulWidget {
+  final VideoPlayerController controller;
+
+  VideoPlayPause(this.controller);
+
+  @override
+  State createState() {
+    return new _VideoPlayPauseState();
+  }
+}
+
+class _VideoPlayPauseState extends State<VideoPlayPause> {
+  FadeAnimation imageFadeAnim =
+      new FadeAnimation(child: const Icon(Icons.play_arrow, size: 100.0));
+  VoidCallback listener;
+
+  _VideoPlayPauseState() {
+    listener = () {
+      setState(() {});
+    };
+  }
+
+  VideoPlayerController get controller => widget.controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(listener);
+    controller.setVolume(1.0);
+    controller.play();
+  }
+
+  @override
+  void deactivate() {
+    controller.setVolume(0.0);
+    controller.removeListener(listener);
+    super.deactivate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> children = <Widget>[
+      new GestureDetector(
+        child: new VideoPlayer(controller),
+        onTap: () {
+          if (!controller.value.initialized) {
+            return;
+          }
+          if (controller.value.isPlaying) {
+            imageFadeAnim =
+                new FadeAnimation(child: const Icon(Icons.pause, size: 100.0));
+            controller.pause();
+          } else {
+            imageFadeAnim = new FadeAnimation(
+                child: const Icon(Icons.play_arrow, size: 100.0));
+            controller.play();
+          }
+        },
+      ),
+      new Align(
+        alignment: Alignment.bottomCenter,
+        child: new VideoProgressIndicator(
+          controller,
+          allowScrubbing: true,
+        ),
+      ),
+      new Center(child: imageFadeAnim),
+    ];
+    return new Stack(
+      fit: StackFit.passthrough,
+      children: children,
+    );
+  }
+}
+
+class FadeAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+
+  FadeAnimation({this.child, this.duration: const Duration(milliseconds: 500)});
+
+  @override
+  _FadeAnimationState createState() => new _FadeAnimationState();
+}
+
+class _FadeAnimationState extends State<FadeAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        new AnimationController(duration: widget.duration, vsync: this);
+    animationController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+    animationController.forward(from: 0.0);
+  }
+
+  @override
+  void deactivate() {
+    animationController.stop();
+    super.deactivate();
+  }
+
+  @override
+  void didUpdateWidget(FadeAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.child != widget.child) {
+      animationController.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return animationController.isAnimating
+        ? new Opacity(
+            opacity: 1.0 - animationController.value,
+            child: widget.child,
+          )
+        : new Container();
+  }
+}
+
+typedef Widget VideoWidgetBuilder(
+    BuildContext context, VideoPlayerController controller);
+
+abstract class PlayerLifeCycle extends StatefulWidget {
+  final VideoWidgetBuilder childBuilder;
+  final String dataSource;
+
+  PlayerLifeCycle(this.dataSource, this.childBuilder);
+}
+
+/// A widget connecting its life cycle to a [VideoPlayerController] using
+/// a data source from the network.
+class NetworkPlayerLifeCycle extends PlayerLifeCycle {
+  NetworkPlayerLifeCycle(String dataSource, VideoWidgetBuilder childBuilder)
+      : super(dataSource, childBuilder);
+
+  @override
+  _NetworkPlayerLifeCycleState createState() =>
+      new _NetworkPlayerLifeCycleState();
+}
+
+abstract class _PlayerLifeCycleState extends State<PlayerLifeCycle> {
+  VideoPlayerController controller;
+
+  @override
+
+  /// Subclasses should implement [createVideoPlayerController], which is used
+  /// by this method.
+  void initState() {
+    super.initState();
+    controller = createVideoPlayerController();
+    controller.addListener(() {
+      if (controller.value.hasError) {
+        print(controller.value.errorDescription);
+      }
+    });
+    controller.initialize();
+    controller.setLooping(false);
+    controller.play();
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.childBuilder(context, controller);
+  }
+
+  VideoPlayerController createVideoPlayerController();
+}
+
+class _NetworkPlayerLifeCycleState extends _PlayerLifeCycleState {
+  @override
+  VideoPlayerController createVideoPlayerController() {
+    return new VideoPlayerController.network(widget.dataSource);
+  }
+}
+
+class AspectRatioVideo extends StatefulWidget {
+  final VideoPlayerController controller;
+
+  AspectRatioVideo(this.controller);
+
+  @override
+  AspectRatioVideoState createState() => new AspectRatioVideoState();
+}
+
+class AspectRatioVideoState extends State<AspectRatioVideo> {
+  VideoPlayerController get controller => widget.controller;
+  bool initialized = false;
+
+  VoidCallback listener;
+
+  @override
+  void initState() {
+    super.initState();
+    listener = () {
+      if (!mounted) {
+        return;
+      }
+      if (initialized != controller.value.initialized) {
+        initialized = controller.value.initialized;
+        setState(() {});
+      }
+    };
+    controller.addListener(listener);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (initialized) {
+      return new Center(
+        child: new AspectRatio(
+          aspectRatio: 16 / 9,
+          child: new VideoPlayPause(controller),
+        ),
+      );
+    } else {
+      return new Container(
+        color: Colors.black,
+        child: new AspectRatio(
+          aspectRatio: 16 / 9,
+          child: const Center(child: const CircularProgressIndicator()),
+        ),
+      );
+    }
+  }
+}
