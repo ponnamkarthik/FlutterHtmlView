8024ba86fe586e064fd66226c6333bbd1fa4b136
nicqc <nicq.chen@outlook.com>
2018-09-15 22:45:40 +0800

    add custom padding setting for html view.

--- lib/flutter_html_view.dart
+++ lib/flutter_html_view.dart
@@ -3,8 +3,9 @@ import 'package:flutter_html_view/html_parser.dart';
 
 class HtmlView extends StatelessWidget {
   final String data;
+  final EdgeInsetsGeometry padding;
 
-  HtmlView({this.data});
+  HtmlView({this.data, this.padding = const EdgeInsets.all(5.0)});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,7 @@ class HtmlView extends StatelessWidget {
     List<Widget> nodes = htmlParser.parseHTML(this.data);
 
     return new Container(
-        padding: const EdgeInsets.all(5.0),
+        padding: padding,
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: nodes,
