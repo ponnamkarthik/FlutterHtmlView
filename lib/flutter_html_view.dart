081bb662bed55cb1115ea934a92fb709b7c0cd88
nicqc <nicq.chen@outlook.com>
2018-09-15 22:54:26 +0800

    add base url option to support some html created by lie editor, such as UEEditor.

--- lib/flutter_html_view.dart
+++ lib/flutter_html_view.dart
@@ -4,15 +4,14 @@ import 'package:flutter_html_view/html_parser.dart';
 class HtmlView extends StatelessWidget {
   final String data;
   final EdgeInsetsGeometry padding;
+  final String baseURL;
 
-  HtmlView({this.data, this.padding = const EdgeInsets.all(5.0)});
+  HtmlView({this.data, this.padding = const EdgeInsets.all(5.0), this.baseURL});
 
   @override
   Widget build(BuildContext context) {
-    HtmlParser htmlParser = new HtmlParser();
-
+    HtmlParser htmlParser = new HtmlParser(this.baseURL);
     List<Widget> nodes = htmlParser.parseHTML(this.data);
-
     return new Container(
         padding: padding,
         child: new Column(
