dbf1e9142fe000d70b0f1699bf61f40970dbb6d9
jianpeng9 <jianpeng9@staff.weibo.com>
2018-11-28 15:29:51 +0800

    htmltext support to change font-size

--- lib/flutter_html_text.dart
+++ lib/flutter_html_text.dart
@@ -505,6 +505,11 @@ class HtmlParser {
 
             break;
 
+          case 'font-size':
+             fontSize = double.parse(value);
+             
+            break;
+
           case 'text-decoration':
             textDecoration = (value == 'underline')
                 ? TextDecoration.underline
