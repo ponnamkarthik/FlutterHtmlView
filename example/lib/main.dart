eb26c91034eb341c15a8d0faa3707fcbfbb886c3
Karthik <ponnamkarthik3@gmail.com>
2019-01-16 10:40:57 +0530

    Done only markdown
    ## [0.5.8]
    
    * cache image network dependency import fixed
    * [Issue #44](https://github.com/PonnamKarthik/FlutterHtmlView/issues/44) Removed print

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,17 +23,21 @@ class _MyAppState extends State<MyApp> {
         <h4>This is heading 4</h4>
         <h5>This is heading 5</h5>
         <h6>This is heading 6</h6>
-        <a href="/some/path">/some/path</a>
+        <a href="https://google.com">Google</a>
+        <br/>
         <img alt="Test Image" src="https://source.unsplash.com/random/300x200">
+        <br/>
         <video src="https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4"></video>
+        <br/>
         <a data-id="1" href="https://google.com">Go Google</a>
+        <br/>
         <a href="mailto:ponnamkarthik3@gmail.com">Mail to me</a>
-        
+        <br/>
         <video id="video1" >
           <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
         </video>
          
-        <img src="/photo-1536816579748-4ecb3f03d72a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=51f0883b2a9f3dacb4501c350c65ce00&auto=format&fit=crop&w=634&q=80">
+        <img src="https://source.unsplash.com/random/300x500">
         ''';
 
     return new MaterialApp(
@@ -41,18 +45,32 @@ class _MyAppState extends State<MyApp> {
         appBar: new AppBar(
           title: new Text('Plugin example app'),
         ),
-        body: new SingleChildScrollView(
-          child: new Center(
-            child: new HtmlView(
-              data: html,
-              baseURL: "https://images.unsplash.com",
-              onLaunchFail: (url) {
-                print("launch $url failed");
-              },
-            ),
+        body: new Container(
+          child: new HtmlView(
+            data: html,
+            stylingOptions: null,
           ),
         ),
       ),
     );
+
+//    return new MaterialApp(
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text('Plugin example app'),
+//        ),
+//        body: new SingleChildScrollView(
+//          child: new Center(
+//            child: new HtmlView(
+//              data: html,
+//              baseURL: "https://images.unsplash.com",
+//              onLaunchFail: (url) {
+//                print("launch $url failed");
+//              },
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
   }
 }
--- lib/flutter_html_view.dart
+++ lib/flutter_html_view.dart
@@ -1,25 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html_view/html_parser.dart';
+
+import 'package:html2md/html2md.dart' as html2md;
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart' as cTab;
+import 'package:url_launcher/url_launcher.dart';
 
 class HtmlView extends StatelessWidget {
   final String data;
-  final TextOverflow overflow;
-  final int maxLines;
-  final EdgeInsetsGeometry padding;
-  final String baseURL;
   final Function onLaunchFail;
+  Map<String, String> stylingOptions;
+  BuildContext ctx;
 
-  HtmlView({this.data, this.padding = const EdgeInsets.all(5.0), this.baseURL, this.onLaunchFail, this.overflow, this.maxLines});
+  HtmlView({this.data, this.stylingOptions, this.onLaunchFail});
 
   @override
   Widget build(BuildContext context) {
-    HtmlParser htmlParser = new HtmlParser(baseUrl: this.baseURL, onLaunchFail: this.onLaunchFail, overflow: this.overflow, maxLines: this.maxLines);
-    List<Widget> nodes = htmlParser.parseHTML(this.data);
-    return new Container(
-        padding: padding,
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: nodes,
-        ));
+    ctx = context;
+    return Markdown(
+        data: _htmlMd(data, stylingOptions),
+      onTapLink: (url) {
+          if (url.startsWith("http://") || url.startsWith("https://")) {
+            _launchURL(url);
+          } else {
+            _launchOtherURL(url);
+          }
+      },
+    );
   }
+
+
+  String _htmlMd(String html, Map<String, String> stylingOptions) {
+    if(stylingOptions != null) {
+      return html2md.convert(html, styleOptions: stylingOptions);
+    } else {
+      return html2md.convert(html);
+    }
+  }
+
+
+  void _launchURL(String url) async {
+    try {
+      await cTab.launch(
+        url,
+        option: new cTab.CustomTabsOption(
+          toolbarColor: Theme.of(ctx).primaryColor,
+          enableDefaultShare: true,
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+        ),
+      );
+    } catch (e) {
+      if (this.onLaunchFail != null) {
+        this.onLaunchFail(url);
+      }
+      debugPrint(e.toString());
+    }
+  }
+
+  void _launchOtherURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      debugPrint('Could not launch $url');
+      if (this.onLaunchFail != null) {
+        this.onLaunchFail(url);
+      }
+    }
+  }
+
 }
